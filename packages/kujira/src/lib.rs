b09b1408f9cc2878a7e798b54bbf573e22d79ee5
bretheren <31092321+bretheren@users.noreply.github.com>
2024-03-04 21:15:35 +0000

    Update cw-multi-test

--- packages/kujira/src/lib.rs
+++ packages/kujira/src/lib.rs
@@ -2,6 +2,7 @@ pub use kujira_bow as bow;
 pub use kujira_fin as fin;
 pub use kujira_ghost as ghost;
 pub use kujira_orca as orca;
+pub use kujira_pilot as pilot;
 pub use kujira_stable as stable;
 
 pub use kujira_std::*;
